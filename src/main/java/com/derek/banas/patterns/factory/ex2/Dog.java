706287c791e422d561ee1ad20368a86fc98784b8
LucashP <pula.lukasz@gmail.com>
2016-04-12 22:48:35 +0200

    Factory pattern - next example

---
+++ src/main/java/com/derek/banas/patterns/factory/ex2/Dog.java
@@ -0,0 +1,5 @@
+package com.derek.banas.patterns.factory.ex2;
+
+public interface Dog {
+    public void speak();
+}
---
+++ src/main/java/com/derek/banas/patterns/factory/ex2/Dogs.java
@@ -0,0 +1,20 @@
+package com.derek.banas.patterns.factory.ex2;
+
+class Poodle implements Dog {
+    public void speak() {
+	System.out.println("The poodle says \"arf\"");
+    }
+}
+
+class Rottweiler implements Dog {
+    public void speak() {
+	System.out
+		.println("The Rottweiler says (in a very deep voice) \"WOOF!\"");
+    }
+}
+
+class SiberianHusky implements Dog {
+    public void speak() {
+	System.out.println("The husky says \"Dude, what's up?\"");
+    }
+}
