bba4924a964eaa4b14259afeea269e2cee02370e
LucashP <pula.lukasz@gmail.com>
2016-05-05 09:45:45 +0200

    Abstract Factory Pattern #1

---
+++ src/com/derek/banas/patterns/abstractfactory/ex1/EnemyShipFactory.java
@@ -0,0 +1,15 @@
+package com.derek.banas.patterns.abstractfactory.ex1;
+
+// With an Abstract Factory Pattern you won't
+// just build ships, but also all of the components
+// for the ships
+
+// Here is where you define the parts that are required
+// if an object wants to be an enemy ship
+
+public interface EnemyShipFactory{
+
+    ESWeapon addESGun();
+    ESEngine addESEngine();
+
+}
