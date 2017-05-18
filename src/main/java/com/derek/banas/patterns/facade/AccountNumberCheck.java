e9e468a5c7015f62ba69ae60118bc34c018be72c
LucashP <pula.lukasz@gmail.com>
2017-05-18 13:34:24 +0200

    Facade pattern

---
+++ src/main/java/com/derek/banas/patterns/facade/AccountNumberCheck.java
@@ -0,0 +1,19 @@
+package com.derek.banas.patterns.facade;
+
+public class AccountNumberCheck {
+
+    private int accountNumber = 12345678;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public boolean accountActive(int acctNumToCheck) {
+        if (acctNumToCheck == getAccountNumber()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
